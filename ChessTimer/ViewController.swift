d48d25cae3e51ff41ae6ee71f4c55bd49588b016
saimanchen <90385724+saimanchen@users.noreply.github.com>
2022-08-30 18:12:04 +0200

    + style: color changes depending on which player plays
    If white plays the background color becomes white and text color becomes black. If black plays the opposite

--- ChessTimer/ViewController.swift
+++ ChessTimer/ViewController.swift
@@ -4,13 +4,15 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var labelWhite: UILabel!
     @IBOutlet weak var labelBlack: UILabel!
+    @IBOutlet weak var labelWhiteTime: UILabel!
+    @IBOutlet weak var labelBlackTime: UILabel!
     @IBOutlet weak var btnPlay: UIButton!
     @IBOutlet weak var btnReset: UIButton!
+    
     
     var countWhite: Double = 300.0
     var countBlack: Double = 300.0
     var isWhite: Bool? = false
-    var isBlack: Bool? = true
     var formatter = DateComponentsFormatter()
     var timer: Timer? = Timer()
     
@@ -21,7 +23,6 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onPressPlay(_ sender: Any) {
-        
         if(btnPlay.titleLabel?.text == "Play again") {
             resetLabels()
         }
@@ -32,11 +33,11 @@ class ViewController: UIViewController {
         // toggles whose turn it is
         if(isWhite ?? false) {
             isWhite = false
-            isBlack = true
         } else {
             isWhite = true
-            isBlack = false
         }
+        
+        updateColor()
         
         if(isWhite ?? false) {
             startTimer(count: countWhite)
@@ -44,6 +45,7 @@ class ViewController: UIViewController {
             startTimer(count: countBlack)
         }
     }
+    
     
     @IBAction func onPressReset(_ sender: Any) {
         timer?.invalidate()
@@ -58,10 +60,10 @@ class ViewController: UIViewController {
     func countDown(timer: Timer?) {
         if(isWhite ?? false) {
             countWhite -= 1
-            labelWhite.text = formatter.string(from: countWhite)
+            labelWhiteTime.text = formatter.string(from: countWhite)
         } else {
             countBlack -= 1
-            labelBlack.text = formatter.string(from: countBlack)
+            labelBlackTime.text = formatter.string(from: countBlack)
         }
         
         if(countWhite == 0 || countBlack == 0) {
@@ -76,13 +78,45 @@ class ViewController: UIViewController {
     }
     
     func resetLabels() {
+        whitesTurn()
         isWhite = false
-        isBlack = true
         countWhite = 300.0
         countBlack = 300.0
         formatter.allowedUnits = [.minute, .second]
-        labelWhite.text = timeFormatter(count: countWhite)
-        labelBlack.text = timeFormatter(count: countBlack)
+        labelWhiteTime.text = timeFormatter(count: countWhite)
+        labelBlackTime.text = timeFormatter(count: countBlack)
+    }
+    
+    func whitesTurn() {
+        view.backgroundColor = UIColor.white
+        labelWhite.textColor = UIColor.black
+        labelBlack.textColor = UIColor.black
+        labelWhiteTime.textColor = UIColor.black
+        labelBlackTime.textColor = UIColor.black
+        btnPlay.setTitleColor(UIColor.white, for: .normal)
+        btnPlay.backgroundColor = UIColor.black
+        btnReset.setTitleColor(UIColor.white, for: .normal)
+        btnReset.backgroundColor = UIColor.black
+    }
+    
+    func blacksTurn() {
+        view.backgroundColor = UIColor.black
+        labelWhite.textColor = UIColor.white
+        labelBlack.textColor = UIColor.white
+        labelWhiteTime.textColor = UIColor.white
+        labelBlackTime.textColor = UIColor.white
+        btnPlay.setTitleColor(UIColor.black, for: .normal)
+        btnPlay.backgroundColor = UIColor.white
+        btnReset.setTitleColor(UIColor.black, for: .normal)
+        btnReset.backgroundColor = UIColor.white
+    }
+    
+    func updateColor() {
+        if(isWhite ?? false) {
+            whitesTurn()
+        } else {
+            blacksTurn()
+        }
     }
     
     func endGame() {
